e3584eb10926dd3558b1acbdbb4886d328ea9923
William Wong <compulim@hotmail.com>
2023-08-06 01:58:57 -0700

    Clean up

--- src/app/ui/DialPad.tsx
+++ src/app/ui/DialPad.tsx
@@ -22,9 +22,26 @@ function deleteFromImmutableSet<T>(set: Set<T>, value: T): Set<T> {
   return nextSet;
 }
 
+function setHasSome<T>(set: Set<T>, anyOf: Iterable<T>): boolean {
+  for (const value of anyOf) {
+    if (set.has(value)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 export default memo(function DialPad({ onButtonClick }: Props) {
   const onButtonClickRef = useRefFrom(onButtonClick);
   const [playingButtons, setPlayingButtons] = useState<Set<DTMFButton>>(new Set());
+  const [startOscillator697, stopOscillator697] = useOscillatorAndGain(697, GAIN);
+  const [startOscillator770, stopOscillator770] = useOscillatorAndGain(770, GAIN);
+  const [startOscillator852, stopOscillator852] = useOscillatorAndGain(852, GAIN);
+  const [startOscillator941, stopOscillator941] = useOscillatorAndGain(941, GAIN);
+  const [startOscillator1209, stopOscillator1209] = useOscillatorAndGain(1209, GAIN);
+  const [startOscillator1336, stopOscillator1336] = useOscillatorAndGain(1336, GAIN);
+  const [startOscillator1477, stopOscillator1477] = useOscillatorAndGain(1477, GAIN);
 
   const handleButton1Click = useCallback(() => onButtonClickRef.current?.('1'), [onButtonClickRef]);
   const handleButton2Click = useCallback(() => onButtonClickRef.current?.('2'), [onButtonClickRef]);
@@ -39,122 +56,51 @@ export default memo(function DialPad({ onButtonClick }: Props) {
   const handleButtonStarClick = useCallback(() => onButtonClickRef.current?.('star'), [onButtonClickRef]);
   const handleButtonPoundClick = useCallback(() => onButtonClickRef.current?.('pound'), [onButtonClickRef]);
 
-  const handleButton1PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '1')),
+  const handlePlayEnd = useCallback(
+    (button: DTMFButton) => setPlayingButtons(set => deleteFromImmutableSet(set, button)),
     [setPlayingButtons]
   );
-  const handleButton1PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('1')),
+  const handlePlayStart = useCallback(
+    (button: DTMFButton) => setPlayingButtons(set => new Set(set).add(button)),
     [setPlayingButtons]
   );
-  const handleButton2PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '2')),
-    [setPlayingButtons]
-  );
-  const handleButton2PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('2')),
-    [setPlayingButtons]
-  );
-  const handleButton3PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '3')),
-    [setPlayingButtons]
-  );
-  const handleButton3PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('3')),
-    [setPlayingButtons]
-  );
-  const handleButton4PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '4')),
-    [setPlayingButtons]
-  );
-  const handleButton4PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('4')),
-    [setPlayingButtons]
-  );
-  const handleButton5PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '5')),
-    [setPlayingButtons]
-  );
-  const handleButton5PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('5')),
-    [setPlayingButtons]
-  );
-  const handleButton6PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '6')),
-    [setPlayingButtons]
-  );
-  const handleButton6PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('6')),
-    [setPlayingButtons]
-  );
-  const handleButton7PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '7')),
-    [setPlayingButtons]
-  );
-  const handleButton7PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('7')),
-    [setPlayingButtons]
-  );
-  const handleButton8PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '8')),
-    [setPlayingButtons]
-  );
-  const handleButton8PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('8')),
-    [setPlayingButtons]
-  );
-  const handleButton9PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '9')),
-    [setPlayingButtons]
-  );
-  const handleButton9PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('9')),
-    [setPlayingButtons]
-  );
-  const handleButton0PlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, '0')),
-    [setPlayingButtons]
-  );
-  const handleButton0PlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('0')),
-    [setPlayingButtons]
-  );
-  const handleButtonStarPlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, 'star')),
-    [setPlayingButtons]
-  );
-  const handleButtonStarPlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('star')),
-    [setPlayingButtons]
-  );
-  const handleButtonPoundPlayEnd = useCallback(
-    () => setPlayingButtons(set => deleteFromImmutableSet(set, 'pound')),
-    [setPlayingButtons]
-  );
-  const handleButtonPoundPlayStart = useCallback(
-    () => setPlayingButtons(set => new Set(set).add('pound')),
-    [setPlayingButtons]
-  );
+
+  const handleButton1PlayEnd = useCallback(() => handlePlayEnd('1'), [setPlayingButtons]);
+  const handleButton1PlayStart = useCallback(() => handlePlayStart('1'), [setPlayingButtons]);
+  const handleButton2PlayEnd = useCallback(() => handlePlayEnd('2'), [setPlayingButtons]);
+  const handleButton2PlayStart = useCallback(() => handlePlayStart('2'), [setPlayingButtons]);
+  const handleButton3PlayEnd = useCallback(() => handlePlayEnd('3'), [setPlayingButtons]);
+  const handleButton3PlayStart = useCallback(() => handlePlayStart('3'), [setPlayingButtons]);
+  const handleButton4PlayEnd = useCallback(() => handlePlayEnd('4'), [setPlayingButtons]);
+  const handleButton4PlayStart = useCallback(() => handlePlayStart('4'), [setPlayingButtons]);
+  const handleButton5PlayEnd = useCallback(() => handlePlayEnd('5'), [setPlayingButtons]);
+  const handleButton5PlayStart = useCallback(() => handlePlayStart('5'), [setPlayingButtons]);
+  const handleButton6PlayEnd = useCallback(() => handlePlayEnd('6'), [setPlayingButtons]);
+  const handleButton6PlayStart = useCallback(() => handlePlayStart('6'), [setPlayingButtons]);
+  const handleButton7PlayEnd = useCallback(() => handlePlayEnd('7'), [setPlayingButtons]);
+  const handleButton7PlayStart = useCallback(() => handlePlayStart('7'), [setPlayingButtons]);
+  const handleButton8PlayEnd = useCallback(() => handlePlayEnd('8'), [setPlayingButtons]);
+  const handleButton8PlayStart = useCallback(() => handlePlayStart('8'), [setPlayingButtons]);
+  const handleButton9PlayEnd = useCallback(() => handlePlayEnd('9'), [setPlayingButtons]);
+  const handleButton9PlayStart = useCallback(() => handlePlayStart('9'), [setPlayingButtons]);
+  const handleButton0PlayEnd = useCallback(() => handlePlayEnd('0'), [setPlayingButtons]);
+  const handleButton0PlayStart = useCallback(() => handlePlayStart('0'), [setPlayingButtons]);
+  const handleButtonStarPlayEnd = useCallback(() => handlePlayEnd('star'), [setPlayingButtons]);
+  const handleButtonStarPlayStart = useCallback(() => handlePlayStart('star'), [setPlayingButtons]);
+  const handleButtonPoundPlayEnd = useCallback(() => handlePlayEnd('pound'), [setPlayingButtons]);
+  const handleButtonPoundPlayStart = useCallback(() => handlePlayStart('pound'), [setPlayingButtons]);
 
   // Playing individual tone is important. This mimick how telephones actually work when multiple buttons are pressed via a crossbar approach.
-  const [startOscillator697, stopOscillator697] = useOscillatorAndGain(697, GAIN);
-  const [startOscillator770, stopOscillator770] = useOscillatorAndGain(770, GAIN);
-  const [startOscillator852, stopOscillator852] = useOscillatorAndGain(852, GAIN);
-  const [startOscillator941, stopOscillator941] = useOscillatorAndGain(941, GAIN);
-  const [startOscillator1209, stopOscillator1209] = useOscillatorAndGain(1209, GAIN);
-  const [startOscillator1336, stopOscillator1336] = useOscillatorAndGain(1336, GAIN);
-  const [startOscillator1477, stopOscillator1477] = useOscillatorAndGain(1477, GAIN);
-
-  const shouldPlayOscillator697 = playingButtons.has('1') || playingButtons.has('2') || playingButtons.has('3');
-  const shouldPlayOscillator770 = playingButtons.has('4') || playingButtons.has('5') || playingButtons.has('6');
-  const shouldPlayOscillator852 = playingButtons.has('7') || playingButtons.has('8') || playingButtons.has('9');
-  const shouldPlayOscillator941 = playingButtons.has('star') || playingButtons.has('0') || playingButtons.has('pound');
-  const shouldPlayOscillator1209 =
-    playingButtons.has('1') || playingButtons.has('4') || playingButtons.has('7') || playingButtons.has('star');
-  const shouldPlayOscillator1336 =
-    playingButtons.has('2') || playingButtons.has('5') || playingButtons.has('8') || playingButtons.has('0');
-  const shouldPlayOscillator1477 =
-    playingButtons.has('3') || playingButtons.has('6') || playingButtons.has('9') || playingButtons.has('pound');
+  const shouldPlayOscillator697 = useMemo(() => setHasSome(playingButtons, ['1', '2', '3']), [playingButtons]);
+  const shouldPlayOscillator770 = useMemo(() => setHasSome(playingButtons, ['4', '5', '6']), [playingButtons]);
+  const shouldPlayOscillator852 = useMemo(() => setHasSome(playingButtons, ['7', '8', '9']), [playingButtons]);
+  const shouldPlayOscillator941 = useMemo(() => setHasSome(playingButtons, ['star', '0', 'pound']), [playingButtons]);
+  const shouldPlayOscillator1209 = useMemo(() => setHasSome(playingButtons, ['1', '4', '7', 'star']), [playingButtons]);
+  const shouldPlayOscillator1336 = useMemo(() => setHasSome(playingButtons, ['2', '5', '8', '0']), [playingButtons]);
+  const shouldPlayOscillator1477 = useMemo(
+    () => setHasSome(playingButtons, ['3', '6', '9', 'pound']),
+    [playingButtons]
+  );
 
   useMemo(() => (shouldPlayOscillator697 ? startOscillator697() : stopOscillator697()), [shouldPlayOscillator697]);
   useMemo(() => (shouldPlayOscillator770 ? startOscillator770() : stopOscillator770()), [shouldPlayOscillator770]);
